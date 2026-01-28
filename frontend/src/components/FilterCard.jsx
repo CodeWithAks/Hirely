8f704ae56d6fe519008ca9d95ce24e880981faf0
Akshara Goyal <meakshara.goyal@gmail.com>
2026-01-28 23:27:45 +0530

    Added filtering logic and debugged errors

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Label } from "@/components/ui/label"
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
 
 const filterData = [
   {
@@ -18,11 +20,22 @@ const filterData = [
 ]
 
 const FilterCard = () => {
+  const [selectedValue,setSelectedValue] = useState("");
+  const dispatch = useDispatch();
+
+  const changeHandler = (value) => {
+    setSelectedValue(value);
+  }
+  useEffect(()=> {
+    dispatch(setSearchedQuery(selectedValue))
+    console.log(selectedValue);
+  },[selectedValue]);
+
   return (
     <div className='w-full bg-white p-3 rounded-md'>
       <h1 className='font-bold text-lg'>Filter Jobs</h1>
       <hr className='mt-3'/>
-      <RadioGroup>
+      <RadioGroup value={selectedValue} onValueChange={changeHandler} >
         {
           filterData.map((data,idx)=>(
             <div>
--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import LatestJobCards from './LatestJobCards.jsx';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 // const randomJobs = [1,2,3,4,5,6,7,8];
 
 const LatestJobs = () => {
   const {allJobs = [] } = useSelector(store=>store.job); //fetching data to get jobs
-
+  
   return (  
     <div className='max-w-7xl mx-auto my-20'>
         <h1 className='text-4xl font-bold'><span className='text-blue-700'>Latest & Top</span> Job Openings</h1>
