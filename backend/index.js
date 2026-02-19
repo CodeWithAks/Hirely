f41dd28e5816253fb1d12fcdfd09de512d23c237
Akshara Goyal <meakshara.goyal@gmail.com>
2026-02-19 17:05:32 +0530

    Deploying app

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:["https://hirely-5cmz.onrender.com","http://localhost:5173"],
     credentials:true
 }
 app.use(cors(corsOptions));
