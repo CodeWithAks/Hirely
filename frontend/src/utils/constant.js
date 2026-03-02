c0f32453c926584003096cd489a5d3ab6497a454
Akshara Goyal <meakshara.goyal@gmail.com>
2026-03-02 14:33:38 +0530

    Final fixes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,3 +2,4 @@ export const USER_API_END_POINT="https://hirely-5cmz.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://hirely-5cmz.onrender.com/api/v1/job"; 
 export const APPLICATION_API_END_POINT="https://hirely-5cmz.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://hirely-5cmz.onrender.com/api/v1/company";
+
