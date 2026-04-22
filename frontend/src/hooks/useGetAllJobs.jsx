6d25fed0253271b17c5efc2115fa19fb7fbb2cba
Akshara Goyal <meakshara.goyal@gmail.com>
2026-04-22 13:56:30 +0530

    fix cross origin issue

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -11,7 +11,7 @@ const useGetAllJobs = () => {
     useEffect(() => {
         const fetchAllJobs = async () => { 
             try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, { withCredentials: true }); 
+                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`); 
                 if (res.data.success) {
                     dispatch(setAllJobs(res.data.jobs)); //to show jobs dynamically
                 }
